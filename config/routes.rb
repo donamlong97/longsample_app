c63a45eccd77c071d6ec89cc6dc19f2963fcb206
donamlong97 <longkute144@gmail.com>
2020-08-04 15:11:09 +0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'users/show'
   get 'static_pages/home'
   get 'static_pages/help'
-  root 'application#hello'# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  root 'static_pages#home'# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
